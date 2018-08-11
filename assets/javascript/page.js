4288ffb283937e85e991ba1614396da353ceb663
Rane M Kaseman <raykman@comcast.net>
2018-08-11 01:51:11 -0600

    0151

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -7,14 +7,12 @@ var topics = [
     ];
     console.log("topics : " + topics);
 
-// var topicsList = topics.replace(/+/gi, " ");
-
 $("button").on("click", function() {
     for (i = 0; i < topics.length; i++) {
-        if (topics[i].includes("+")) {
-            topics[i].replace(/"+"/g, " t ");
-        }
-        $("#buttons").append($("<button>").text(topics[i]));
+        // if (topics[i].includes("+")) {
+        //     topics[i].replace(/\W/, " t ");
+        // }
+        $('#buttons').append($("<button>").text(topics[i]));
         console.log("topics[i] : " + topics[i]);
     };
     $.ajax({
@@ -22,11 +20,11 @@ $("button").on("click", function() {
         method: "GET"
     }).then(function(response) {
         var results = response.data;
-        console.log(response.data);
         console.log(response.data[0].images);
         console.log(response.data[1].images);
         console.log(response.data[2].images);
 
     });
+    $("#ownDamnGif").append("<form id='bigForm'><p><input id='bigField type='text' name='search' value='Get your own damn gif'></p><p><input id='btn' type='submit' value='Submit'></p></form>");
 });
 
