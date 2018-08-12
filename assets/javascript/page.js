34e6a47e44a008cbc3a864f5081943a973a0a1eb
Rane M Kaseman <raykman@comcast.net>
2018-08-11 23:25:37 -0600

    2325

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -7,33 +7,38 @@ var topicArr = [
     "women"
     ];
     console.log("topicArr : " + topicArr);
+    console.log("----onLoad----");
 
-$("button").on("click", function() {
-    $("#start").hide();
+$("#begin").on("click", function() {
+    $("#begin").hide();
 
     for (i = 0; i < topicArr.length; i++) {
         // if (topicArr[i].includes("+")) {
         //     topicArr[i].replace(/\W/, " t ");
         // }
-        $("#buttons").append($("<button class='btnGen' data-topic=" + topicArr[i] + ">").text(topicArr[i]));
-        console.log("this = button : " + this);
+        $("#buttons").append($("<button class='btnGen' data-topic="+topicArr[i]+">").text(topicArr[i]));
+        console.log(".this '#begin' : " + this);
         console.log("topicArr[i] : " + topicArr[i]);
+        console.log("----1----");
+    }; // end for i
 
     $(".btnGen").on("click", function() {
+        $("#gifs").empty();
         var dataTopicTest = $(this).attr("data-topic")
         console.log("dataTopicTest : " + dataTopicTest);
-        var btnURLs = "http://api.giphy.com/v1/gifs/search?q=" + dataTopicTest + "&api_key=" + giphyAPI + "&limit=5";
-        console.log("this = btnGen : " + this);
+        var btnURLs = "http://api.giphy.com/v1/gifs/search?q="+dataTopicTest+"&api_key="+giphyAPI+"&limit=5";
+        console.log("this '.btnGen' : " + this);
         console.log("btnURLs : " + btnURLs);
+        console.log("----2----");
 
     $.ajax({
         url: btnURLs,
         method: "GET"
-    })
+    }) // end $.ajax
         .then(function(response) {
             console.log(response.data);
-            // console.log(response.data[0].images);
             for (j = 0; j < response.data.length; j++) {
+                console.log("response.data.length = " + response.data.length);
                 var p = $("<p>").text("Rating Test " + response.data[j].rating);
                 var topicGif = $("<img>");
                 topicGif.attr("src", response.data[j].images.fixed_height.url);
@@ -42,7 +47,6 @@ $("button").on("click", function() {
             }; // end for j
         }); //end .then
     }); // end .btnGen click
-    }; // end for i
 
     $("#ownDamnGif").append("<form id='bigForm'><div><input id='bigField type='text' name='search' value='Get your own damn gif'></div><div><input id='btn' type='submit' value='Submit'></div></form>");
 
