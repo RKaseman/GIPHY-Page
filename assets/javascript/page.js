a434b83d451125e81f925f2ea7ad47c24d679ec5
Rane M Kaseman <raykman@comcast.net>
2018-08-12 22:28:41 -0600

    2228

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -8,67 +8,70 @@ var topicArr = [
     "women+celebrities",
     "women+video+games",
 ];
-    console.log("topicArr : " + topicArr);
+    console.log("topicArr set : " + topicArr);
 
 $("#begin").on("click", function () {
     $("#begin").hide();
     $("h3").hide();
 
-    for (i = 0; i < topicArr.length; i++) {
-        // if (topicArr[i].includes("+")) {
-        //     topicArr[i].replace(/"+"/g, " t ");
-        // }
-        $("#buttons").append($("<button class='btnGen' data-topic=" + topicArr[i] + ">").text(topicArr[i]));
-        console.log("topicArr[i] : " + topicArr[i]);
-    }; // end for i
+    userBtn();
+    function userBtn() {
+        $("#buttons").empty();
+        for (i = 0; i < topicArr.length; i++) {
+            // if (topicArr[i].includes("+")) {
+            //     topicArr[i].replace(/"+"/g, " t ");
+            // }
+            $("#buttons").append($("<button class='btnGen' data-topic=" + topicArr[i] + ">").text(topicArr[i]));
+            console.log("topicArr[i] : " + topicArr[i]);
+        }; // end for i
 
-    $(".btnGen").on("click", function () {
-        $("#gifs").empty();
-        var dataTopic = $(this).attr("data-topic")
-        console.log("dataTopic : " + dataTopic);
-        var btnURLs = "https://api.giphy.com/v1/gifs/search?q=" + dataTopic + "&api_key=" + giphyAPI + "&limit=5&offset=5";
-        console.log("btnURLs : " + btnURLs);
+        $(".btnGen").on("click", function () {
+            $("#gifs").empty();
+            var dataTopic = $(this).attr("data-topic")
+            console.log("dataTopic : " + dataTopic);
+            var btnURLs = "https://api.giphy.com/v1/gifs/search?q=" + dataTopic + "&api_key=" + giphyAPI + "&limit=5&offset=5";
+            console.log("btnURLs : " + btnURLs);
 
-    $.ajax({
-        url: btnURLs,
-        method: "GET"
-    }) // end $.ajax
-        .then(function (response) {
-            console.log(response.data);
-            for (j = 0; j < response.data.length; j++) {
-                console.log("response.data.length = " + response.data.length);
-                var p = $("#gifs").append("<p>Rated " + response.data[j].rating + "</p>");
-                var topicGif = $("<img>");
-                topicGif.attr("src", response.data[j].images.fixed_height_still.url);
-                topicGif.attr("data-still", response.data[j].images.fixed_height_still.url);
-                topicGif.attr("data-animate", response.data[j].images.fixed_height.url);
-                topicGif.attr("data-state", "still");
-                topicGif.attr("class", "gif");
-                $("#gifs").append(p, topicGif);
-            }; // end for j
+        $.ajax({
+            url: btnURLs,
+            method: "GET"
+        }) // end $.ajax
+            .then(function (response) {
+                console.log(response.data);
+                for (j = 0; j < response.data.length; j++) {
+                    console.log("response.data.length = " + response.data.length);
+                    var p = $("#gifs").append("<p>Rated " + response.data[j].rating + "</p>");
+                    var topicGif = $("<img>");
+                    topicGif.attr("src", response.data[j].images.fixed_height_still.url);
+                    topicGif.attr("data-still", response.data[j].images.fixed_height_still.url);
+                    topicGif.attr("data-animate", response.data[j].images.fixed_height.url);
+                    topicGif.attr("data-state", "still");
+                    topicGif.attr("class", "gif");
+                    $("#gifs").append(p, topicGif);
+                }; // end for j
 
-            $(".gif").on("click", function () {
-                console.log("this : " + this);
-                var dataState = $(this).attr("data-state");
-                if (dataState === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                }
-                    else {
-                        $(this).attr("src", $(this).attr("data-still"));
-                        $(this).attr("data-state", "still");
+                $(".gif").on("click", function () {
+                    console.log("this : " + this);
+                    var dataState = $(this).attr("data-state");
+                    if (dataState === "still") {
+                        $(this).attr("src", $(this).attr("data-animate"));
+                        $(this).attr("data-state", "animate");
                     }
-            }); // end animation toggle
-        }); //end .then
-    }); // end .btnGen click
-
+                        else {
+                            $(this).attr("src", $(this).attr("data-still"));
+                            $(this).attr("data-state", "still");
+                        }
+                }); // end animation switch state
+            }); //end .then
+        }); // end .btnGen click
+    }; // end userBtn
         $("#ownDamnGif").append(
             "<form id='wholeForm' action='#' method='post'>" 
                 + "<div>" 
                     + "<label for='search'>Search GIPHY</label>" 
                 + "</div>" 
                 + "<div>" 
-                    + "<input id='bigField' type='text' name='search' value='' placeholder='and get your own damn gif'>" 
+                    + "<input type='text' id='bigField' name='search' value='' placeholder='and get your own damn gif'>" 
                 + "</div>" 
                 + "<div>" 
                     + "<input id='btn' type='submit' value='Submit'>" 
@@ -76,11 +79,12 @@ $("#begin").on("click", function () {
             + "</form>");
 
         $("#btn").on("click", function(event) {
-            var searchValue = $("#bigField").attr("value");
+            var searchValue = document.querySelector("#bigField").value;
             console.log("searchValue : " + searchValue);
-
+            topicArr.push("women+" + searchValue);
+            console.log("topicArr .push : " + topicArr);
             event.preventDefault();
-            console.log(this);
+            userBtn();
         });
     });
 
